86548afc12c2e57d6ad032474ea3e48f0ddd9e8a
Ankur Gupta <ankuratudemy@gmail.com>
2020-08-19 04:26:28 +0530

    added all files

--- sensors/checkwebsitesensor.py
+++ sensors/checkwebsitesensor.py
@@ -19,7 +19,7 @@ class CheckWebsiteSensor(Sensor):
 
     def run(self):
         while not self._stop:
-            self._logger.debug(f'[checkwebsite sensor] Checking {self._webiste_url} status... ')
+            self._logger.info(f'[checkwebsite sensor] Checking {self._webiste_url} status... ')
             # sending get request and saving the response as response object 
             try:
                 r = requests.get(url = self._website_url)

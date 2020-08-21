c33f45a6d5099516d20561e5cb834f3aa8af8119
Ankur Gupta <ankuratudemy@gmail.com>
2020-08-21 16:44:58 +0530

    added all files

--- actions/add.py
+++ actions/add.py
@@ -4,9 +4,6 @@ from st2common.runners.base_action import Action
 
 class addAction(Action):
     def run(self, operand1,operand2):
-        print(operand1)
-        print(operand2)
-
         if operand1 and operand2:
             self.logger.info('Add action successfully completed with operand1 = %s and operand2= %d' % (operand1,operand2))
             return (True, operand1+operand2)
--- actions/multiply.py
+++ actions/multiply.py
@@ -4,12 +4,9 @@ from st2common.runners.base_action import Action
 
 class multiplyAction(Action):
     def run(self, operand1,operand2):
-        print(operand1)
-        print(operand2)
-
         if operand1 and operand2:
-            self.logger.info('Add action successfully completed with operand1 = %s and operand2= %d' % (operand1,operand2))
+            self.logger.info('Multiply action successfully completed with operand1 = %s and operand2= %d' % (operand1,operand2))
             return (True, operand1 * operand2)
         else:
-            self.logger.error('Add action failed with operand1 = %s and operand2= %d' % (operand1,operand2))
+            self.logger.error('Multiply action failed with operand1 = %s and operand2= %d' % (operand1,operand2))
             return (False, 0)
